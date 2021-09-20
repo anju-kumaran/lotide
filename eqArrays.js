f4d2bdd7e36d2a796a9565327b5fc9419c4ca537
anju-kumaran <anju.naduth@gmail.com>
2021-09-20 16:10:16 -0400

    Refatctored eqArrays by seperating the test functions

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const eqArrays = function(inputArr1, inputArr2) {
   let result = false;
   for ( let i = 0; i < inputArr1.length; i++) {
@@ -17,7 +10,4 @@ const eqArrays = function(inputArr1, inputArr2) {
   return result;
  };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
+ module.exports = eqArrays;
