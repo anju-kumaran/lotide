2811660d163cec36a15c207710dfeedb5fa8b644
anju-kumaran <anju.naduth@gmail.com>
2021-09-20 20:08:53 -0400

    Refactored tail with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,32 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
 
-// Test Case 2: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  // Test Case 1: Check the returned array elements
+  const result = tail(["Hello", "Lighthouse", "Labs"]);
+  it("returns Labs for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(result[1], 'Labs');
+  });
 
-// Test Case 3: Array with only one element should yield an empty array for its tail 
-const singleArr = tail(["Lighthouse"]); 
-assertEqual(singleArr.length, 0); // 0 elements!
+  // Test Case 2: Check the original array 
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it("returns Labs for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    tail(words); // no need to capture the return value since we are not checking it
+    assert.deepEqual(words.length, 3); // original array should still have 3 elements!
+  });
+  
+  // Test Case 3: Array with only one element should yield an empty array for its tail 
+  const singleArr = tail(["Lighthouse"]); 
+  it("returns 0 for ['Lighthouse']", () => {
+    assert.deepEqual(singleArr.length, 0); // 0 elements!
+  });
 
-// Test Case 4:  Empty array should yield an empty array for its tail
-const emptyArr = tail([]); 
-assertEqual(emptyArr.length, 0); // 0 elements!
+  //Test Case 4:  Empty array should yield an empty array for its tail
+  const emptyArr = tail([]); 
+  it("returns 0 for []", () => {
+    assert.deepEqual(emptyArr.length, 0); // 0 elements!
+  });
+  
+
+});
